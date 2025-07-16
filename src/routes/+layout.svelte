894d72183e423df266e422f41d9c6704a435dbca
Joy <joykarmakar987654321@gmail.com>
2025-07-17 00:11:08 +0530

    removed data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 <script lang="ts">
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
-
-	export let data;
 </script>
 
 <main class="min-h-screen bg-gray-50 font-sans text-gray-800">
