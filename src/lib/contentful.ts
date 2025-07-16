9268f580842b0b799fc95b5ca2d316f556372d2d
Joy <joykarmakar987654321@gmail.com>
2025-07-17 00:30:15 +0530

    update

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,7 +1,11 @@
 import { env } from '$env/dynamic/private';
 import { createClient } from 'contentful';
 
+if (!env.CONTENTFUL_SPACE_ID || !env.CONTENTFUL_ACCESS_TOKEN) {
+    throw new Error('Missing Contentful environment variables.');
+}
+
 export const client = createClient({
-    space: env.CONTENTFUL_SPACE_ID || '',
-    accessToken: env.CONTENTFUL_ACCESS_TOKEN || '',
+    space: env.CONTENTFUL_SPACE_ID,
+    accessToken: env.CONTENTFUL_ACCESS_TOKEN,
 });
