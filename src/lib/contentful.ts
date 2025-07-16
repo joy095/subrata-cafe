b3f938ae25fe79a20c6bab2c7072a8354dc00fe0
Joy <joykarmakar987654321@gmail.com>
2025-07-17 00:27:36 +0530

    updated with server side env

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,6 +1,7 @@
+import { env } from '$env/dynamic/private';
 import { createClient } from 'contentful';
 
 export const client = createClient({
-    space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+    space: env.CONTENTFUL_SPACE_ID || '',
+    accessToken: env.CONTENTFUL_ACCESS_TOKEN || '',
 });
