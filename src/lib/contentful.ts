61384466cccc2100989792c7cf97a89e1f61c3e3
Joy <joykarmakar987654321@gmail.com>
2025-07-17 10:24:07 +0530

    rename

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,8 +1,5 @@
 import { createClient } from 'contentful';
 
-if (!import.meta.env.VITE_CONTENTFUL_SPACE_ID || !import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN) {
-    throw new Error('Missing Contentful environment variables.');
-}
 
 export const client = createClient({
     space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
